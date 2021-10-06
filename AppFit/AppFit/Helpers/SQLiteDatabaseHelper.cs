7757ea91188efe4dc389d3d257a9dc274df92ac7
Felipe Geroldi <42786299+felipegeroldi@users.noreply.github.com>
2021-10-06 00:34:14 -0300

    Desenvolvimento da Shell, criação de rotas das tabs inferiores

---
+++ AppFit/AppFit/Helpers/SQLiteDatabaseHelper.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppFit.Helpers
+{
+    class SQLiteDatabaseHelper
+    {
+    }
+}
---
+++ AppFit/AppFit/ViewModels/CadastroAtividadeViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppFit.ViewModels
+{
+    class CadastroAtividadeViewModel
+    {
+    }
+}
